72c01d539312970826044f32a8870e9ccba543db
Daniel Hedayati <daniel.hedayati@sage.com>
2018-05-25 08:24:38 -0700

    Initial Commit

---
+++ JsonValidator/JsonValidator/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+namespace JsonValidator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidate_Click(object sender, RoutedEventArgs e)
+        {
+            var validator = new SchemaValidator();
+            validator.jsonFullFileName = txtInputFile.Text;
+            validator.SchemaPath = txtSchemaPath.Text;
+            IEnumerable<string> errors = validator.Validate();
+
+
+            Dispatcher.Invoke(() =>
+            {
+                if (errors != null && errors.Count() > 0)
+                    foreach (var error in errors)
+                        lstError.Items.Add(error);
+                else
+                    lstError.Items.Add("No Error");
+            });
+
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnInputFilePicker_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                Dispatcher.Invoke(() =>
+                {
+                    txtInputFile.Text = openFileDialog.FileName;
+                });
+
+        }
+
+        private void btnSchemaFolderPicker_Click(object sender, RoutedEventArgs e)
+        {
+            var openFolderDialog = new FolderBrowserDialog();
+
+
+            if (openFolderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                Dispatcher.Invoke(() =>
+                {
+                    txtSchemaPath.Text = openFolderDialog.SelectedPath;
+                });
+        }
+    }
+}
+
---
+++ JsonValidator/JsonValidator/SchemaValidator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Linq;
+
+namespace JsonValidator
+{
+    public class SchemaValidator
+    {
+        public string jsonFullFileName { get; set; }
+
+        private string _schemaPath;
+        public string SchemaPath
+        {
+            get
+            {
+                return _schemaPath;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    if (value.Substring(value.Length - 1, 1) != @"\")
+                        _schemaPath = value + @"\";
+                    else
+                        _schemaPath = value;
+                }
+            }
+        }
+
+
+        public IList<string> Validate()
+        {
+            if (string.IsNullOrEmpty(SchemaPath) || string.IsNullOrEmpty(jsonFullFileName))
+                return null;
+
+            IList<string> errors = new List<string>();
+            //  using (StreamReader reader = File.OpenText(@"C:\Users\dhedayati\Documents\Visual Studio 2017\Projects\Repos\JsonValid\JsonValid\BDPSchema\business.json"))
+            var parentSchema = Path.Combine(SchemaPath, "business.json");
+            if (!File.Exists(parentSchema))
+            {
+                //System.Windows.MessageBox.Show("Schema file cannot be found.","Error");
+                errors.Add("Schema file cannot be found");
+                return errors;
+            }
+
+            using (StreamReader reader = File.OpenText(@"C:\Users\dhedayati\Documents\Visual Studio 2017\Projects\Repos\JsonValid\JsonValid\BDPSchema\business.json"))
+            {
+                using (JsonTextReader jsonReader = new JsonTextReader(reader))
+                {
+                    JSchemaUrlResolver resolver = new JSchemaUrlResolver();
+                    JSchema schema = JSchema.Load(jsonReader, new JSchemaReaderSettings()
+                    {
+                        Resolver = resolver,
+
+                        //BaseUri = new Uri(@"file://C:/Users/dhedayati/Documents/Visual Studio 2017/Projects/Repos/JsonValid/JsonValid/BDPSchema/")
+
+                        BaseUri = new Uri(SchemaPath)
+                    });
+
+                    string data = ReadJsonFile();
+                    var jTok = JToken.Parse(data);
+                    jTok.IsValid(schema, out errors);
+                }
+            }
+            return errors;
+        }
+
+        public string ReadJsonFile()
+        {
+            using (StreamReader reader = new StreamReader(@"C:\SageFinancialsDataDrop\Universl.json"))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
